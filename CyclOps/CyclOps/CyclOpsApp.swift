4bffafdd83a4c6409d37dfebe81b45a22ccb2940
rwhite53 <rwhite@october-design.com>
2024-06-04 12:09:27 -0400

    New Project default files

---
+++ CyclOps/CyclOps/CyclOpsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CyclOpsApp.swift
+//  CyclOps
+//
+//  Created by Ron White on 6/4/24.
+//
+
+import SwiftUI
+
+@main
+struct CyclOpsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
